956e8c2e896f2d79577f3fe8da23caaae25af235
alnutile <alfrednutile@gmail.com>
2015-06-07 09:40:13 -0400

    Fix sqlite

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -9,3 +9,13 @@
 	composer dump-autoload
 	php artisan migrate --force
 @endtask
+
+@task('dev_deploy')
+	cd /home/forge/default
+	git reset --hard HEAD
+	git pull origin production
+	rm -rf vendor
+	composer install
+	composer dump-autoload
+    php artisan migrate:refresh --seed
+@endtask
